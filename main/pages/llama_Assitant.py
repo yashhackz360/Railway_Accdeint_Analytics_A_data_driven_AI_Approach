e94e551c59dbdff5481712185cadbedb4e47bfa8
Yashwanth sai kasarabada <163118932+yashhackz360@users.noreply.github.com>
2025-07-03 14:53:07 +0530

    Update llama_Assitant.py

--- main/pages/llama_Assitant.py
+++ main/pages/llama_Assitant.py
@@ -11,7 +11,7 @@ def show():
     @st.cache_data
     def load_data():
         try:
-            df = pd.read_csv("main/Assets/train_Accident_1900_2024_cleaned.xlsx")
+            df = pd.read_csv("Assests/train_Accident_1900_2024_cleaned.csv")
             return df
         except FileNotFoundError:
             st.error("⚠️ Railway accident dataset not found!")
